e1e0c55def7e0a184bdf555ba74e2b49380d94af
sato <ysudqfs@163.com>
2021-08-30 15:01:59 +0800

    type fix

--- include/use_struct.cpp
+++ include/use_struct.cpp
@@ -9,7 +9,7 @@ void compare() {
     unsigned unit_sold;
     my_double venue;
     if (salesData1.BookNo == salesData2.BookNo) {
-        unit_sold = salesData1.unit_sold + salesData2.venue;
+        unit_sold = salesData1.unit_sold + salesData2.unit_sold;
         venue = salesData1.venue + salesData2.venue;
     }
     std::cout << unit_sold << "\t" << venue << std::endl;
